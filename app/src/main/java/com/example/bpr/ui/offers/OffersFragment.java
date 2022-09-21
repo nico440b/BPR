4667fa215f49e05f76ef117cc2960897504a4fdf
nfioavu <ioanaoctavia00@yahoo.com>
2022-09-21 14:41:47 +0200

    template for Offers fragment

--- app/src/main/java/com/example/bpr/ui/offers/OffersFragment.java
+++ app/src/main/java/com/example/bpr/ui/offers/OffersFragment.java
@@ -27,13 +27,13 @@ public class OffersFragment extends Fragment {
         binding = FragmentOffersBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textOffers;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 
