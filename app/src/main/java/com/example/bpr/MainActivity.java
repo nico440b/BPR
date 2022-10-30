6a95a6f7bb4b8a0d28cf7c003a4e45153b2cadce
nfioavu <ioanaoctavia00@yahoo.com>
2022-10-30 13:31:35 +0100

    manual combine martin's and nicolai's commits

--- app/src/main/java/com/example/bpr/MainActivity.java
+++ app/src/main/java/com/example/bpr/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bpr;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,25 +10,52 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Header;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.room.Room;
 
 import com.example.bpr.databinding.ActivityMainBinding;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private String email,password;
+    private ActivityMainBinding binding;
+    public static Context context;
+    private NetworkImpl network = new NetworkImpl();
+    private CoopStoreCore core = new CoopStoreCore();
+    private Converters converters = new Converters();
+    private List<CoopProducts> coopProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +81,28 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        context = getApplicationContext();
+        //core = network.CoopStoreAPI();
+        coopProducts = network.CoopProductsAPI();
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "databasetest").allowMainThreadQueries().fallbackToDestructiveMigration().build();
+
+        CoopStoreDao coopStoreDao = db.coopStoreDao();
+        CoopProductsDao coopProductsDao = db.coopProductsDao();
+         /*
+         coopStoreDao.insertAll(core);
+         CoopStoreCore testCore = new CoopStoreCore();
+         testCore = coopStoreDao.getAll();
+         Log.e("Rest Respone", "testCore.apiVersion");
+         Log.e("Rest Respone", testCore.apiVersion);
+
+          */
+        coopProductsDao.insertAll(coopProducts);
+        List<CoopProducts> test;
+
+        test = coopProductsDao.getAll();
+        //Log.e("Rest Respone", test.get(0).navn);
     }
     private void signIn(String email,String password){
         mAuth.signInWithEmailAndPassword(email, password)
